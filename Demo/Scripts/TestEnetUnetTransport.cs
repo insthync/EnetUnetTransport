616e1a4fb38372ef51faec00ae0a19d1924ff7c4
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-09-17 03:55:39 +0700

    Update demo content

---
+++ Demo/Scripts/TestEnetUnetTransport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class TestEnetUnetTransport : MonoBehaviour
+{
+    public static TestEnetUnetTransport Instance { get; private set; }
+
+    private void OnEnable()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        try
+        {
+            Debug.Log("Setup transport to custom transport");
+            //NetworkManager.activeTransport = new TestLLAPITransport();
+            NetworkManager.activeTransport = new EnetUnetTransport();
+            SceneManager.LoadScene("DemoLiteNetLibUnetTransport");
+        }
+        catch (Exception ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+}
---
+++ Demo/Scripts/TestEnetUnetTransportCharacter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestEnetUnetTransportCharacter : NetworkBehaviour
+{
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(transform.forward);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(-transform.forward);
+            }
+        }
+    }
+}
